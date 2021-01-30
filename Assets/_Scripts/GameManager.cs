e30fefeaa145b916177b1d21fc3883268a3aa1c8
Patrick Rotter <p.rotter@wt.de>
2021-01-30 02:06:46 +0100

    [ADDED] game result

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     
-    internal void RemovePrey(Prey preyToRemove) { _prey.Remove(preyToRemove); }
-    
     [SerializeField] private float roundDuration;
+    [SerializeField] private int scoreToWin;
+    
+    [Header("InputObjects")]
     [SerializeField] private TMP_Text timer;
     [SerializeField] private TMP_Text score;
+    [SerializeField] private TMP_Text result;
+    [SerializeField] private GameObject endScreen;
     
-    private List<Prey> _prey;
-    private List<Hunter> _hunters;
     private int _score;
     private float _timer;
+    private bool _hasPlayer;
 
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        _hasPlayer = true;
+    }
+    
     private void Awake()
     {
         #region Singleton
@@ -29,19 +37,24 @@ public class GameManager : MonoBehaviour
         
         DontDestroyOnLoad(this);
         #endregion
-        
-        _prey = new List<Prey>();
-        _hunters = new List<Hunter>();
-    }
 
+        timer.text = roundDuration.ToString("00");
+
+        GetComponent<PlayerInputManager>().onPlayerJoined += OnPlayerJoined;
+    }
+    
     private void Update()
     {
+        if (!_hasPlayer) return;
+        
         roundDuration -= Time.deltaTime;
 
         timer.text = Convert.ToInt32(roundDuration).ToString("00");
         
-        if(Convert.ToInt32(roundDuration) == 0)
-            EndRound();
+        if(_score >= scoreToWin)
+            EndRound(true);
+        else if(Convert.ToInt32(roundDuration) == 0)
+            EndRound(false);
     }
 
     internal void SetScore(int amount)
@@ -50,8 +63,19 @@ public class GameManager : MonoBehaviour
         score.text = _score.ToString("000");
     }
     
-    private void EndRound()
+    private void EndRound(bool gameResult)
     {
-        Debug.Log("END");
+        endScreen.SetActive(true);
+        Time.timeScale = 0;
+
+        switch (gameResult)
+        {
+            case true:
+                result.text = "Mice win!";
+                break;
+            case false:
+                result.text = "Mice lose.";
+                break;
+        }
     }
 }
