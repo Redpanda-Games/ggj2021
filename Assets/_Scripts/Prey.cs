64d7b12a584e621da763a178d62dc33990ec28b3
Patrick Rotter <p.rotter@wt.de>
2021-01-30 02:06:18 +0100

    [REMOVED] unused parts

--- Assets/_Scripts/Prey.cs
+++ Assets/_Scripts/Prey.cs
@@ -2,27 +2,10 @@
 
 public class Prey : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(!other.gameObject.CompareTag("Hunter")) return;
         
         Destroy(gameObject);
-    }
-
-    private void OnDestroy()
-    {
-        GameManager.Instance.RemovePrey(this);
     }
 }
