7a0d010235d4a7da203ae1f551e4311bffca87a6
Patrick Rotter <p.rotter@wt.de>
2021-01-31 11:41:19 +0100

    [ADDED] death squeak to prey prefab

--- Assets/_Scripts/Prey.cs
+++ Assets/_Scripts/Prey.cs
@@ -2,10 +2,24 @@
 
 public class Prey : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] sounds;
+    private AudioSource _audio;
+
+    private void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(!other.gameObject.CompareTag("Hunter")) return;
         
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if(!_audio.isPlaying)
+            _audio.PlayOneShot(sounds[0]);
     }
 }
